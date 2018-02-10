50080c770c5fe4be1c1b2627058c5555f3af61a7
ZKY199202 <32631293+ZKY199202@users.noreply.github.com>
2018-02-10 11:15:19 +0800

    Update rpcwallet.cpp

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -5,7 +5,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "sodium.h"
+//#include "sodium.h"
 #include "amount.h"
 #include "base58.h"
 #include "chain.h"
