7894806fa70309f2054cfa9a06ff114a7dc1e72b
camphor <my_camphor_tree@163.com>
2018-03-05 15:56:04 +0800

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -1,4 +1,3 @@
-
 // Copyright (c) 2012-2014 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -43,8 +42,7 @@ const std::string CLIENT_NAME("Ulord Core");
 #include "build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
 #ifdef GIT_ARCHIVE
 #define GIT_COMMIT_ID "$Format:%h$"
 #define GIT_COMMIT_DATE "$Format:%cD$"
