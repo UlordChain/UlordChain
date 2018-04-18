4b73fa79fe2813b870c8a1eea98fde802d149a1a
Armin <sumingrui@gmail.com>
2018-04-18 19:22:48 +0800

    Update common.c

--- src/hello/common.c
+++ src/hello/common.c
@@ -6,12 +6,6 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-/*
- * 功能：字节数组打印方法
- * 输入：1. mess：字节数组名称
- *		 2. data：字节数组地址
- *		 3. len ：字数组长度
-*/
 void view_data_u8(const char *mess, 
 		uint8_t *data, uint32_t len) {
 	printf("%-18s\t", mess);
@@ -20,16 +14,29 @@ void view_data_u8(const char *mess,
 	printf("\n");
 }
 
-/*
- * 功能：字数组打印方法
- * 输入：1. mess：字数组名称
- *		 2. data：字数组地址
- *		 3. len ：字数组长度
-*/
 void view_data_u32(const char *mess, 
 		uint32_t *data, uint32_t len) {
 	printf("%s: ", mess);
 	for (uint32_t i = 0; i < len; ++i)
 		printf("%.8x ", data[i]);
 	printf("\n");
+}
+
+void reduce_bit(uint8_t *input, uint32_t inputLen, 
+        uint8_t *output, uint32_t bits) {                                                                                                                                                                         
+    uint32_t i, outputLen = (bits) >> 3;
+    memcpy(output, input, outputLen * sizeof(uint8_t));
+    for (i = outputLen; i < inputLen; ++i) {
+        output[i % outputLen] ^= input[i % inputLen];
+    }
+}
+void rrs(uint8_t *input, uint32_t inputLen, 
+        uint8_t *output, uint32_t bits) {
+    uint32_t shiftBytes = (bits) >> 3, shiftBits = (bits) & 0x7;
+    uint32_t rIndex = (inputLen) - shiftBytes;
+    uint32_t lIndex = (rIndex + (inputLen) - 1) % (inputLen);
+    for (uint32_t i = 0; i < inputLen; ++i) {
+        output[i] = (input[(rIndex++) % (inputLen)] >> shiftBits) |
+            (input[(lIndex++) % (inputLen)] << (8 - shiftBits));
+    }
 }
