896b43e382e21e66f1b726d1f4921168d9416153
Armin <sumingrui@gmail.com>
2018-04-18 19:26:33 +0800

    Update common.h

--- src/hello/common.h
+++ src/hello/common.h
@@ -32,24 +32,10 @@ inline void reduce_bit_2(uint8_t *input, uint32_t inputLen,
 }
 
 inline void reduce_bit(uint8_t *input, uint32_t inputLen, 
-		uint8_t *output, uint32_t bits) {
-	uint32_t i, outputLen = (bits) >> 3;
-	memcpy(output, input, outputLen * sizeof(uint8_t));
-	for (i = outputLen; i < inputLen; ++i) {
-		output[i % outputLen] ^= input[i % inputLen];
-	}
-}
+		uint8_t *output, uint32_t bits);
 
 inline void rrs(uint8_t *input, uint32_t inputLen, 
-		uint8_t *output, uint32_t bits) {
-	uint32_t shiftBytes = (bits) >> 3, shiftBits = (bits) & 0x7;
-	uint32_t rIndex = (inputLen) - shiftBytes;
-	uint32_t lIndex = (rIndex + (inputLen) - 1) % (inputLen);
-	for (uint32_t i = 0; i < inputLen; ++i) {
-		output[i] = (input[(rIndex++) % (inputLen)] >> shiftBits) |
-			(input[(lIndex++) % (inputLen)] << (8 - shiftBits));
-	}
-}
+		uint8_t *output, uint32_t bits);
 
 #ifdef __cplusplus
 extern "C" {
