32d726f0dba9653f49e5efb478d0f06b418a2694
AlfredZKY <17630302151@163.com>
2018-05-17 17:02:26 +0800

    add a function of crosschain

--- src/script/script.cpp
+++ src/script/script.cpp
@@ -224,6 +224,37 @@ bool CScript::IsNormalPaymentScript() const
     return true;
 }
 
+bool CScript::IsCrossChainPaymentScript() const
+{
+    if(this->size() != 81) return false;
+
+    std::string str;
+    opcodetype opcode;
+    const_iterator pc = begin();
+    int i = 0;
+    while (pc < end())
+    {
+        GetOp(pc, opcode);
+
+		if( 	i == 0 && opcode != OP_IF) return false;
+		else if(i == 1 && opcode != OP_RIPEMD160) return false;
+		else if(i == 3 && opcode != OP_EQUALVERIFY) return false;
+		else if(i == 4 && opcode != OP_DUP) return false;
+		else if(i == 5 && opcode != OP_HASH160) return false;
+		else if(i == 7 && opcode != OP_ELSE) return false;
+		else if(i == 9 && opcode != OP_CHECKLOCKTIMEVERIFY) return false;
+		else if(i == 10 && opcode != OP_DROP) return false;
+		else if(i == 11 && opcode != OP_DUP) return false;
+		else if(i == 12 && opcode != OP_HASH160) return false;
+		else if(i == 14 && opcode != OP_ENDIF) return false;
+		else if(i == 15 && opcode != OP_EQUALVERIFY) return false;
+		else if(i == 16 && opcode != OP_CHECKSIG) return false;
+		else if(i == 17) return false;
+        i++;
+    }
+
+    return true;
+}
 bool CScript::IsPayToPublicKeyHash() const
 {
     // Extra-fast test for pay-to-pubkey-hash CScripts:
