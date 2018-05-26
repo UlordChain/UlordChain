9957d71296f64d27069234ff3f67429ad36f2144
Kwuaint <34888408+kwuaint@users.noreply.github.com>
2018-05-26 10:46:00 +0800

    Change the head def
    Change the head def

--- src/wallet/crypter.h
+++ src/wallet/crypter.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_WALLET_CRYPTER_H
-#define BITCOIN_WALLET_CRYPTER_H
+#ifndef ULORD_WALLET_CRYPTER_H
+#define ULORD_WALLET_CRYPTER_H
 
 #include "keystore.h"
 #include "serialize.h"
@@ -217,4 +217,4 @@ public:
     boost::signals2::signal<void (CCryptoKeyStore* wallet)> NotifyStatusChanged;
 };
 
-#endif // BITCOIN_WALLET_CRYPTER_H
+#endif // ULORD_WALLET_CRYPTER_H
