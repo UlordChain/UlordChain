6039586c1b8e749be32d4f2f93abafdad5a505f0
kwuaint <king_lear@tom.com>
2018-05-28 09:21:01 +0800

    fix the messy code of the function desc

--- src/hello/c_whirlpool.c
+++ src/hello/c_whirlpool.c
@@ -7,9 +7,9 @@
 #include "common.h"
 
 /*
-     * FUNCTION：one-way function Whirlpool
+     * FUNCTION: one-way function Whirlpool
      *      1. input : input message, msglength
-     *		2. output：Whirlpool message
+     *		2. output: Whirlpool message
 */
 void crypto_whirlpool(uint8_t *input, uint32_t inputLen, uint8_t *output) {
 	uint8_t result[WHIRLPOOL_DIGEST_LENGTH];
--- src/hello/c_whirlpool.h
+++ src/hello/c_whirlpool.h
@@ -8,9 +8,9 @@ extern "C" {
 #endif
 
 	/*
-     * FUNCTION：one-way function Whirlpool
+     * FUNCTION: one-way function Whirlpool
      *      1. input : input message, msglength
-     *		2. output：Whirlpool message
+     *		2. output: Whirlpool message
 	*/
 	void crypto_whirlpool(uint8_t *input, uint32_t inputLen, uint8_t *output);
 
