1b46ff0e0e85546136d4426200a26bb00f54f983
alvin <li_alvin@qq.com>
2018-06-09 17:32:51 +0800

    Update compressor.cpp

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -8,7 +8,7 @@
 #include "hash.h"
 #include "pubkey.h"
 #include "script/standard.h"
-
+// judge is pay to  public key hash 
 bool CScriptCompressor::IsToKeyID(CKeyID &hash) const
 {
     if (script.size() == 25 && script[0] == OP_DUP && script[1] == OP_HASH160
