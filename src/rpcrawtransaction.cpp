ff93989980d185fda4749bb13c65a2dca004212f
liangqi1983 <65593062@qq.com>
2018-06-12 09:19:11 +0800

    get the key of recipient address

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -1113,6 +1113,12 @@ UniValue crosschainredeem(const UniValue &params, bool fHelp)
 		{
 			return JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Error:Can't find the pubkey of participte address");			
 		}
+	if(!keystore.GetKey(keyID,key))
+		{
+			return JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Error:Can't find the key of participte address");	
+		}
+
+
 
     return result;
 }
