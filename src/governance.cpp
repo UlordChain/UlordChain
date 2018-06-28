fcff842d0b8514624bf194d56a9e3f1fba58468d
alvin <li_alvin@qq.com>
2018-06-28 15:27:35 +0800

    Update governance.cpp

--- src/governance.cpp
+++ src/governance.cpp
@@ -741,7 +741,7 @@ bool CGovernanceManager::MasternodeRateCheck(const CGovernanceObject& govobj, bo
     bool fRateCheckBypassed = false;
     return MasternodeRateCheck(govobj, fUpdateLast, true, fRateCheckBypassed);
 }
-
+// rate check ,trigger can not great than twice per superblockcycle , watchdog can not great than twice per 3600 second
 bool CGovernanceManager::MasternodeRateCheck(const CGovernanceObject& govobj, bool fUpdateLast, bool fForce, bool& fRateCheckBypassed)
 {
     LOCK(cs);
