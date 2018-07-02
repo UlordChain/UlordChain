97408b19c749b784e8a3cd5fc8e3b318a36135d8
liangqi1983 <65593062@qq.com>
2018-07-02 19:58:25 +0800

    get the key of refund address

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -1354,6 +1354,10 @@ UniValue crosschainrefund(const UniValue &params, bool fHelp)
 		{
 			return JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Error:Can't find the pubkey of participte address");			
 		}
+	if(!keystore.GetKey(keyID,key))
+		{
+			return JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Error:Can't find the key of participte address");	
+		}
 
     return result;
 }
