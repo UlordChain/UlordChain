7d08ce4cd503d2dd99f0c494270bf7886625b428
bicliu <klins@163.com>
2018-07-11 14:38:59 +0800

    format

--- src/main.cpp
+++ src/main.cpp
@@ -1237,11 +1237,11 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState &state, const C
         CAmount nValueOut = tx.GetValueOut();
         CAmount nFees = nValueIn-nValueOut;
         // nModifiedFees includes any fee deltas from PrioritiseTransaction 
-	//  minTxFee  GetMinimumFee，  next step add mempool will judge pool fee
-	if(nFees<=0) 
-	{
-	   return state.DoS(0, false, REJECT_INSUFFICIENTFEE, "insufficient nFees");
-	}    
+	    //  minTxFee  GetMinimumFee, next step add mempool will judge pool fee
+	    if(nFees<=0) 
+	    {
+	        return state.DoS(0, false, REJECT_INSUFFICIENTFEE, "insufficient nFees");
+	    }    
         CAmount nModifiedFees = nFees;
         double nPriorityDummy = 0;
         pool.ApplyDeltas(hash, nPriorityDummy, nModifiedFees);
