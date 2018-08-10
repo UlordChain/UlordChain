e2409d173369429f1a42dd36ba1fc28bd0ef738e
dingchao <369755377@qq.com>
2018-08-10 12:05:11 +0800

    Modify conf type name

--- src/masternodeconfig.cpp
+++ src/masternodeconfig.cpp
@@ -16,64 +16,67 @@ void CMasternodeConfig::add(std::string alias, std::string ip, std::string privK
 
 bool CMasternodeConfig::read(std::string& strErr) {
 
-    std::string alias, ip, privKey, txHash, outputIndex;
-    int64_t masternodecout = GetArg("-masternode", 0);  
-    alias = GetArg("-alias", "mnl");
-    if(alias.empty() && masternodecout)
-    {
-        strErr = _("please add your masternode name into ulord.conf; for example: alias=mynode\n");
-    }
-    ip = GetArg("-externalip", "");
-    if(ip.empty() && masternodecout)
-    {
-        strErr = _("Invalid masternode ip, please add your ip into ulord.conf; for example: externalip=0.0.0.0\n");
-        return false;
-    }
-    ip = ip + ":" + std::to_string(Params().GetDefaultPort());
-    
-    privKey = GetArg("-masternodeprivkey", "");
-    if(privKey.empty() && masternodecout)
-    {
-        strErr = _("Invalid masternode privKey, please add your privKey into ulord.conf; for example: masternodeprivkey=***\n");
-        return false;
-    }
-    txHash = GetArg("-collateraloutputtxid", "");
-    if(txHash.empty() && masternodecout)
-    {
-        strErr = _("Invalid masternode collateral txid, please add your collateral txid into ulord.conf; for example: collateraloutputtxid=***\n");
-        return false;
-    }
 
-    outputIndex = GetArg("-collateraloutputindex", "");
-    if(outputIndex.empty() && masternodecout)
+    int64_t masternodecout = GetArg("-masternode", 0);  
+    if(masternodecout)
     {
-        strErr = _("Invalid masternode collateral Index, please add your collateral Index into ulord.conf; for example: collateraloutputindex=0\n");
-        return false;
-    }
-    
-    int port = 0;
-    std::string hostname = "";
-    SplitHostPort(ip, port, hostname);
-    if(port == 0 || hostname == "") {
-        strErr = _("Failed to parse host:port string") + "\n";
-        return false;
-    }
-    int mainnetDefaultPort = Params(CBaseChainParams::MAIN).GetDefaultPort();
-    if(Params().NetworkIDString() == CBaseChainParams::MAIN) {
-        if(port != mainnetDefaultPort) {
-            strErr = _("Invalid port detected in ulord.conf") + "\n" +
-                    strprintf(_("Port: %d"), port) + "\n" +
-                    strprintf(_("(must be %d for mainnet)"), mainnetDefaultPort);
+        std::string alias, ip, privKey, txHash, outputIndex;
+        alias = GetArg("-alias", "mnl");
+        if(alias.empty())
+        {
+            strErr = _("please add your masternode name into ulord.conf; for example: alias=mynode\n");
+        }
+        ip = GetArg("-externalip", "");
+        if(ip.empty())
+        {
+            strErr = _("Invalid masternode ip, please add your ip into ulord.conf; for example: externalip=0.0.0.0\n");
             return false;
         }
-    } else if(port == mainnetDefaultPort) {
-        strErr = _("Invalid port detected in ulord.conf") + "\n" +
-                strprintf(_("(%d could be used only on mainnet)"), mainnetDefaultPort);
-        return false;
-    }
+        ip = ip + ":" + std::to_string(Params().GetDefaultPort());
         
-    add(alias, ip, privKey, txHash, outputIndex);
+        privKey = GetArg("-masternodeprivkey", "");
+        if(privKey.empty())
+        {
+            strErr = _("Invalid masternode privKey, please add your privKey into ulord.conf; for example: masternodeprivkey=***\n");
+            return false;
+        }
+        txHash = GetArg("-collateraloutputtxid", "");
+        if(txHash.empty())
+        {
+            strErr = _("Invalid masternode collateral txid, please add your collateral txid into ulord.conf; for example: collateraloutputtxid=***\n");
+            return false;
+        }
 
+        outputIndex = GetArg("-collateraloutputindex", "");
+        if(outputIndex.empty())
+        {
+            strErr = _("Invalid masternode collateral Index, please add your collateral Index into ulord.conf; for example: collateraloutputindex=0\n");
+            return false;
+        }
+        
+        int port = 0;
+        std::string hostname = "";
+        SplitHostPort(ip, port, hostname);
+        if(port == 0 || hostname == "") {
+            strErr = _("Failed to parse host:port string") + "\n";
+            return false;
+        }
+        int mainnetDefaultPort = Params(CBaseChainParams::MAIN).GetDefaultPort();
+        if(Params().NetworkIDString() == CBaseChainParams::MAIN) {
+            if(port != mainnetDefaultPort) {
+                strErr = _("Invalid port detected in ulord.conf") + "\n" +
+                        strprintf(_("Port: %d"), port) + "\n" +
+                        strprintf(_("(must be %d for mainnet)"), mainnetDefaultPort);
+                return false;
+            }
+        } else if(port == mainnetDefaultPort) {
+            strErr = _("Invalid port detected in ulord.conf") + "\n" +
+                    strprintf(_("(%d could be used only on mainnet)"), mainnetDefaultPort);
+            return false;
+        }
+            
+        add(alias, ip, privKey, txHash, outputIndex);
+    }
     return true;
 }
 
