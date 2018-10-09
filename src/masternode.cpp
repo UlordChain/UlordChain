e151b3568ca228586465468e299b6355397c6bd4
alvin <li_alvin@qq.com>
2018-10-10 07:58:21 +0800

    Update masternode.cpp

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -804,7 +804,7 @@ bool CMasternodeBroadcast::CheckOutpoint(int& nDos)
     //int64_t tespPeriod= certifyPeriod;
     if(certifyPeriod < lastPing.certifyPeriod)
     {
-        LogPrintf("CMasternodeBroadcast::CheckOutpoint warning certificate, =[%ld] period=[%ld]\n", lastPing.certifyPeriod,tespPeriod);
+        LogPrintf("CMasternodeBroadcast::CheckOutpoint warning certificate,lastping =[%ld] period=[%ld]\n", lastPing.certifyPeriod,certifyPeriod);
         certifyPeriod= lastPing.certifyPeriod;
         certifyVersion= lastPing.certifyVersion;
         certificate = lastPing.certificate;
