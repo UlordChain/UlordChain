84c24a36f318e34bf54d3ff86b0c0f594bce11d1
tkblack <blacketanke@qq.com>
2018-11-14 14:01:39 +0800

    add PoSeBan function

--- src/masternode.h
+++ src/masternode.h
@@ -300,7 +300,7 @@ public:
 
     void IncreasePoSeBanScore() { if(nPoSeBanScore < MASTERNODE_POSE_BAN_MAX_SCORE) nPoSeBanScore++; }
     void DecreasePoSeBanScore() { if(nPoSeBanScore > -MASTERNODE_POSE_BAN_MAX_SCORE) nPoSeBanScore--; }
-
+	void PoSeBan() { nPoSeBanScore = MASTERNODE_POSE_BAN_MAX_SCORE; }
     masternode_info_t GetInfo();
 
     static std::string StateToString(int nStateIn);
