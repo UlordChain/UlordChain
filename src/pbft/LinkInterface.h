b3da54506bbd273e119fb0f4a8d27ea2e419c1ca
alvin <lialvin@qq.com>
2018-12-15 14:38:12 +0800

    link manager

---
+++ src/pbft/LinkInterface.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "DatabaseTypes.h"
+#include <boost/signals2.hpp>
+
+namespace Pbft {
+
+class LinkInterface
+{
+public:
+    LinkInterface(const LinkInterface&) = delete;
+    virtual ~LinkInterface() = default;
+
+    LinkInterface& operator=(const LinkInterface&) = delete;
+
+    virtual void Send(const Message& message) const = 0;
+
+    ::boost::signals2::signal<void(const Message&)> Receive;
+
+protected:
+    LinkInterface() = default;
+};
+
+}
