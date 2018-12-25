da47378fe5d5908e3edf0926cd18da9b026e6c51
alvin <lialvin@qq.com>
2018-12-25 14:57:04 +0800

    modify conn

--- src/pbft/Connection.h
+++ src/pbft/Connection.h
@@ -3,14 +3,14 @@
 #include "DatabaseInterface.h"
 
 namespace Pbft {
-
+// dpos connect node
 class Connection
 {
 public:
     explicit Connection(DatabaseInterface& databaseToSet);
 
     virtual DatabaseInterface& Database() const;
-
+    bool    isConnected;
 private:
     DatabaseInterface& database;
 };
